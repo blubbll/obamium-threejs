8d17db1665e13ba992e907992d52b5e39e950fbb
Glitch (obamium-threejs) <none>
2020-01-31 05:30:02 +0000

    add obasnake

--- script.js
+++ script.js
@@ -43,6 +43,33 @@ var o = new THREE.Mesh(geometry, material);
 //add the cube to the scene, defaults to coords 0,0,0 (x,y,z)
 scene.add(o);
 
+let l;
+{
+  function CustomSinCurve(scale) {
+    THREE.Curve.call(this);
+
+    this.scale = scale === undefined ? 1 : scale;
+  }
+
+  CustomSinCurve.prototype = Object.create(THREE.Curve.prototype);
+  CustomSinCurve.prototype.constructor = CustomSinCurve;
+
+  CustomSinCurve.prototype.getPoint = function(t) {
+    var tx = t * 3;
+    var ty = Math.sin(2 * Math.PI * t);
+    var tz = 1;
+
+    return new THREE.Vector3(tx, ty, tz).multiplyScalar(this.scale);
+  };
+
+  var path = new CustomSinCurve(10);
+  var geometry = new THREE.TubeGeometry(path, 20, 2, 8, false);
+  //var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+  var mesh = new THREE.Mesh(geometry, material);
+  scene.add(mesh);
+  l = mesh;
+}
+
 //move the camera back a bit so it is not sitting on the cube, if you see a white canvas try adjusting the camera pos
 camera.position.z = 20;
 
@@ -51,6 +78,9 @@ var render = function() {
   requestAnimationFrame(render);
   o.rotation.x += 0.0;
   o.rotation.y += 0.05;
+
+  l.rotation.x += 0.002;
+  l.rotation.y += 0.025;
 
   renderer.render(scene, camera);
 };
